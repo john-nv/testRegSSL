d3314e7cb1f12ab145795cb28e4332c8fa7f375d
john <quangngo1500@gmail.com>
2024-05-09 22:10:38 +0700

    feat

--- index.js
+++ index.js
@@ -1,43 +1,103 @@
+const { exec } = require('child_process');
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
 const forge = require('node-forge');
 const fs = require('fs');
 
-// const privateKeyData = fs.readFileSync('ECgenpkey.key', 'utf8');
-// const privateKey = forge.pki.privateKeyFromAsn1(privateKeyData)
+const PORT = 3301
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
-// // Tạo một CSR
-// const csr = forge.pki.createCertificationRequest();
-// csr.publicKey = privateKey.publicKey;
+server.listen(PORT, () => { console.log(`Server is running on ${PORT}`) })
 
-// // Thiết lập các trường trong chứng chỉ yêu cầu
-// csr.setSubject([
-//     { name: 'countryName', value: 'DE' },
-//     { name: 'stateOrProvinceName', value: 'Bavaria' },
-//     { name: 'localityName', value: 'Vehicle' },
-//     { name: 'organizationName', value: 'BMW AG' },
-//     { name: 'organizationalUnitName', value: 'Head Unit' },
-//     { name: 'commonName', value: '6749050753D90095C9E603CA1A7E5F0D4A524E8AB0C277145AE6F7556FB37BE8' }
-// ]);
+app.use(express.urlencoded({ extended: true }))
 
-// // Ký chứng chỉ yêu cầu bằng khóa riêng tư
-// csr.sign(privateKey);
+app.post('/regKey', async  (req, res) => { 
+  const { CA } = req.body;
+  let CRT1_content = ''
+  let CRT2_content = ''
+  let token_content = ''
+  try {
+    // await commandExec()
 
-// // Lưu chứng chỉ yêu cầu vào một tệp
-// const csrData = forge.pki.certificationRequestToPem(csr);
-// fs.writeFileSync('OK.csr', csrData);
+    CRT1_content = fs.readFileSync('./CRT1.crt', 'utf8');
+    CRT2_content = fs.readFileSync('./CRT2.crt', 'utf8');
 
+    if (CRT2_content.length < 10) {
+      console.log('Nội dung CRT2 không hợp lệ');
+      return res.status(200).json({ status: false, CRT2: undefined, token: undefined });
+    }
 
-const { exec } = require('child_process');
+    token_content = CRT2_content + CRT1_content
+    // console.log(token_content)
 
-const command1 = `openssl req -new -sha256 -key ECgenpkey.key -out OK.csr -subj "/C=DE/ST=Bavaria/L=Vehicle/O=BMW AG/OU=Head Unit/CN=6749050753D90095C9E603CA1A7E5F0D4A524E8AB0C277145AE6F7556FB37BE8"`;
+    const hexToken = stringToHex(token_content);
+    console.log(hexToken)
 
-const command2 = `openssl x509 -req -in OK.csr -CA CRT1.crt -CAkey privateKeyCRT1.pem -out CRT2.crt -sha256 -days 22995`;
+    return res.status(200).json({ status: true, CRT2: CRT2_content, token: token_content })
+  } catch (error) {
+    console.log(error)
+  }
+})
 
+function stringToHex(str) {
+  let hex = '';
+  for (let i = 0; i < str.length; i++) {
+    hex += str.charCodeAt(i).toString(16).padStart(2, '0');
+  }
+  return hex;
+}
 
-setTimeout(()=>{
-  exec(command1);
-},1000)
+async function commandExec(){
+    console.log("time-before")
+    exec('sudo date 051111111970');
 
-setTimeout(()=>{
-  exec(command2);
-},5000)
+    await sleep(500)
+    console.log("OK.crs starting...")
+    exec(`openssl req -new -sha256 -key ECgenpkey.key -out OK.csr -subj "/C=DE/ST=Bavaria/L=Vehicle/O=BMW AG/OU=Head Unit/CN=${CN}"`);
+    
+    await sleep(1000)
+    console.log("CRT2.crt starting...")
+    exec('openssl x509 -req -in OK.csr -CA CRT1.crt -CAkey privateKeyCRT1.pem -out CRT2.crt -sha256 -days 24820 -extfile extensions.cnf -extensions x509_ext');
+    
+    await sleep(1000)
+    console.log("time-after")
+    exec('sudo date 051111112024');
+}
+
+async function sleep(ms) {
+    await new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// openssl x509 -req -in OK.csr -CA CRT1.crt -CAkey privateKeyCRT1.pem -out CRT2.crt -sha256 -days 24820 -extfile extensions.cnf -extensions x509_ext
+// openssl x509 -req -in OK.csr -CA CRT1.crt -CAkey privateKeyCRT1.pem -out CRT2.crt -sha256 -days 24820 -extfile extensions.cnf -extensions x509_ext -config template.conf
+// openssl x509 -req -in OK.csr -CA CRT1.crt -CAkey privateKeyCRT1.pem -out CRT2.crt -sha256 -days 24820 -config template.conf
+// openssl x509 -req -in OK.csr -CA CRT1.crt -CAkey privateKeyCRT1.pem -out CRT2.crt -days 24820 -extfile extensions.cnf -extensions x509_ext -extfile template.conf
+// openssl x509 req -extfile validity.ext -in OK.csr -out extended_OK.csr
+// openssl x509 -text -noout -in CRT2.crt
+
+// sudo timedatectl set-ntp on
+// sudo date 051111111970
+
+// setTimeout(()=>{
+//   console.log("time-before")
+//   exec('sudo date 051111111970');
+// },500)
+
+// setTimeout(()=>{
+//   console.log("OK.crs starting...")
+//   exec(`openssl req -new -sha256 -key ECgenpkey.key -out OK.csr -subj "/C=DE/ST=Bavaria/L=Vehicle/O=BMW AG/OU=Head Unit/CN=${CN}"`);
+// },500)
+
+// setTimeout(()=>{
+//   console.log("CRT2.crt starting...")
+//   exec('openssl x509 -req -in OK.csr -CA CRT1.crt -CAkey privateKeyCRT1.pem -out CRT2.crt -sha256 -days 24820 -extfile extensions.cnf -extensions x509_ext');
+// },2000)
+
+// setTimeout(()=>{
+//   console.log("time-after")
+//   exec('sudo date 051111112024');
+// },2500)
